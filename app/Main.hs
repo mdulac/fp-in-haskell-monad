1111068ec3821973bfd6d993e8c895ee0aafb894
mdulac <mdulac@gmail.com>
2015-12-06 19:18:42 +0100

    Remove solution

--- app/Main.hs
+++ app/Main.hs
@@ -15,52 +15,36 @@ class Monade m where
 	flatMap :: m a -> (a -> m b) -> m b
 	-- EXO 2
 	map :: m a -> (a -> b) -> m b
-	map ma f = flatMap ma (\a -> point $ f a)
+	map ma f = error "todo"
 	ap :: m a -> m (a -> b) -> m b
-	ap ma mf = flatMap ma (\a -> map mf (\f -> f a))
+	ap ma mf = error "todo"
 	-- EXO 3
 	flatten :: m (m a) -> m a
-	flatten mma = flatMap mma id
+	flatten mma = error "todo"
 
 -- EXO 4
 instance Monade List where
-	point x = Cons x Nil
-	flatMap Nil f = Nil
-	flatMap (Cons h t) f = f h `concat` flatMap t f
+	point x = error "todo"
+	flatMap Nil f = error "todo"
+	flatMap (Cons h t) f = error "todo"
 
 instance Monade Box where
-	point = Full
-	flatMap Empty f = Empty
-	flatMap (Full v) f = f v
+	point = error "todo"
+	flatMap Empty f = error "todo"
+	flatMap (Full v) f = error "todo"
 
 -- EXO 9
 composeListAndBox :: List (Box a) -> (a -> b) -> List (Box b)
-composeListAndBox l f = (fmap . fmap) f l
+composeListAndBox l f = error "todo"
 
 composeBoxAndList :: Box (List a) -> (a -> b) -> Box (List b)
-composeBoxAndList l f = (fmap . fmap) f l
+composeBoxAndList l f = error "todo"
 
 -- EXO 8
-composeF :: (Functor f1, Functor f2) => (a -> b) -> f1 (f2 a) -> f1 (f2 b)
-composeF = fmap . fmap
+-- TODO
 
 -- EXO 10
-data Writer a = Writer {value :: a, logg :: String} deriving (Show)
-
-instance Functor Writer where
-	fmap f (Writer v l) = Writer (f v) l
-
-instance Applicative Writer where
-	pure v = Writer v ""
-	(<*>) (Writer f l) fa = fmap f fa
-
-instance Monad Writer where
-	return v = Writer v ""
-	(>>=) (Writer v l) f =
-		let w = f v in Writer (value w) (logg w ++ l) 
-
-write :: a -> String -> Writer a
-write = Writer
+-- TODO
 
 main :: IO ()
 main = do
@@ -101,27 +85,7 @@ main = do
 
 	print $ Full 6 >>= (\x -> Full (x + 10) >>= (\y -> Full (y + 100)))
 
-	print $ do
-		x <- box 6
-		y <- return (x + 10)
-		z <- if (y > 15) then Empty else return y
-		return (z + 100)
-
-	print $ do
-		func <- list [(+1), (+2), (+3)]
-		e <- list [10, 11, 12]
-		return $ func e
-
-	print $ do
-		v <- box 34
-		v1 <- box $ v + 12
-		return v1
-
-	print $ do
-		v <- box 34
-		v1 <- box $ v + 12
-		v2 <- Empty
-		return v1 :: Box Int
+	-- TODO
 
 -}
 {-
@@ -135,14 +99,7 @@ main = do
 -}
 {-
 	-- EXO 9
-
-	value <- getLine
-	print $ "Hello " ++ (fmap toUpper value)
-
-	x <- getLine
-	y <- getLine
-	z <- getLine
-	print $ x ++ y ++ z
+	-- TODO
 
 -}
 {-
